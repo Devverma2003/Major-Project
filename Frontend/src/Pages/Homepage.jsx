48b955aa846b584c815e25d799a5c3f260ffe0b4
[Dev Verma] <[devverma98765@gmail.com]>
2025-05-02 12:38:02 +0530

    code updated

---
+++ Frontend/src/Pages/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
+import Hero from '../Components/Hero/Hero'
+import Cards from '../Components/Cards/Cards'
+
+
+const Homepage = () => {
+  return (
+    <div>
+    <Navbar/>
+    <Hero/>
+    <Cards/>
+    </div>
+  )
+}
+
+export default Homepage
