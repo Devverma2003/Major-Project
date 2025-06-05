4beab7d4fc6b6add96e9b0496a2c93fb9d558a55
[Dev Verma] <[devverma98765@gmail.com]>
2025-06-05 11:30:37 +0530

    Login-sigup added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,8 @@ import BookNow from "./pages/Destination/BookNow";
 import Categories from './Pages/Categories/Categories';
 import Wildlife from './Components/Wildlife/Wildlife';
 import Aboutpage from "./Pages/Aboutpage";
+import Signup from './Pages/Signup-login/Signup';
+import Login from './Pages/Signup-login/Login';
 
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
       <Route path="/" element={<Homepage />} />
       <Route path="/destination" element={<Destination />} />
       <Route path="/book-now" element={<BookNow />} />
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/login" element={<Login/>} />
       <Route path="/Aboutpage" element={<Aboutpage />} />
       <Route path="/Categories" element={<Categories />} />
       <Route path="/Categories/Wildlife" element={<Wildlife />} />
